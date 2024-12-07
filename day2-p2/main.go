01d5944f9511f57e816fc940b8c93df770158170
Meiram <Subudei@gmail.com>
2024-12-07 22:47:50 +0500

    day2

---
+++ day2-p2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	count := 0
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		sl := strings.Split(line, " ")
+		nums := make([]int, len(sl))
+
+		for i, v := range sl {
+			num, _ := strconv.Atoi(v)
+			nums[i] = num
+		}
+
+		if isSafe(nums) {
+			count++
+
+			continue
+		}
+
+		for i := 0; i < len(nums); i++ {
+			cp := make([]int, 0, len(nums))
+			cp = append(cp, nums[:i]...)
+			cp = append(cp, nums[i+1:]...)
+
+			if isSafe(cp) {
+				count++
+
+				break
+			}
+		}
+	}
+
+	fmt.Println("count: ", count)
+}
+
+func isSafe(sl []int) bool {
+	dir := ""
+	for i := 0; i < len(sl)-1; i++ {
+		if dir == "" {
+			if sl[i] == sl[i+1] || abs(sl[i], sl[i+1]) > 3 {
+				return false
+			}
+
+			if sl[i] < sl[i+1] {
+				dir = "inc"
+			} else if sl[i] > sl[i+1] {
+				dir = "decr"
+			}
+		} else {
+			switch dir {
+			case "inc":
+				if sl[i] >= sl[i+1] || abs(sl[i], sl[i+1]) > 3 {
+					return false
+				}
+			case "decr":
+				if sl[i] <= sl[i+1] || abs(sl[i], sl[i+1]) > 3 {
+					return false
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+func abs(n1, n2 int) int {
+	if n2 > n1 {
+		return n2 - n1
+	}
+
+	return n1 - n2
+}
